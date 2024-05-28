5119882617610acdf8c06ad46f6bf59c806dd9f6
MVKarthikReddy <karthikmedagam@gmail.com>
2024-05-29 00:00:16 +0530

    some changes

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://rentify-gules.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
