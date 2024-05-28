d4acefc3e1cbcedd0e5fd466efefb418060e4c99
MVKarthikReddy <karthikmedagam@gmail.com>
2024-05-28 23:29:41 +0530

    some changes

--- server/server.js
+++ server/server.js
@@ -39,8 +39,10 @@ app.listen(5000,() => {
   // Serve static files if we are in production
   app.use(express.static(path.join(__dirname,'/client/dist')));
   // Handling any routes that don't match the ones above (Should be at the end)
-  app.get('*', (req, res) =>{
-    res.sendFile(path.join(__dirname,'client', 'dist','index.html'));//__dirname + '/views/index.html
+  app.get('/', (req, res) =>{
+    res.json({
+      message : "Backend is working fine"
+    })
   })
       
   //we create a middleware to handles error  if they are not caught by other middlewares or handlers
