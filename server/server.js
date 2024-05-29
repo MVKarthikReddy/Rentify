21c95a758c672c918a1105099541f5a1022284a9
MVKarthikReddy <karthikmedagam@gmail.com>
2024-05-29 12:10:11 +0530

    some changes

--- server/server.js
+++ server/server.js
@@ -11,13 +11,19 @@ require('dotenv').config();
 
 const app = express();
 
+// Middleware to set CORS headers
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://rentify-gules.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://rentify-gules.vercel.app'); // Allow specific origin
+  res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); // Handle preflight requests
+  }
+  
   next();
 });
-
 // app.use(cookieParser());
 // app.use(cors());
         
