00fa70230479cf5689839064d47eeeafdd3984cf
MVKarthikReddy <karthikmedagam@gmail.com>
2024-07-18 14:15:55 +0530

    some routing problems are solved

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -2,11 +2,12 @@ const jwt = require('jsonwebtoken');
 const errorHandler = require('./error.js');
 
 const verifyToken = (req, res, next) => {
+  // console.log("hello")
   const token = req.headers['authorization'].split(' ')[1];
   // console.log(req.headers)
   // console.log(token)
   if (!token) {
-    // console.log(token)
+    // console.log("I am Working Fine")
     return next(errorHandler(401, 'Unauthorized'));
   }
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
